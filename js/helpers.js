8d5354866d4efd91110e2b9e08538f52fd3276c8
marncz <m.czarnecki1@uni.brighton.ac.uk>
2017-10-16 08:28:06 +0100

    User's location not working on servers without https enabled.

--- js/helpers.js
+++ js/helpers.js
@@ -27,14 +27,16 @@ var initMap = function () {
     zoom: 4
   });
 
-  if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function (position) {
-         initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-         map.setCenter(initialLocation);
-         map.setZoom(15);
-     });
- }
-
+  if ((location.hostname === "localhost" || location.hostname === "127.0.0.1" ) || location.protocol == 'https:')
+    if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function (position) {
+           initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+           map.setCenter(initialLocation);
+           map.setZoom(15);
+       });
+    }
+  }
+  
   map.addListener('click', function(e) {
     clearOverlays();
     placeMarkerAndPanTo(e.latLng, map);
